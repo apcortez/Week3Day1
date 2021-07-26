e68207dba1e6a0c377e7b9a5c56f05d55d0b1ccc
Angelica Cortez <angelica.cortez@hotmail.it>
2021-07-26 14:37:31 +0200

    Esercizio 2: Verifica carta - bug non finito

---
+++ Esercizio2/CardManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio2
+{
+    class CardManager
+    { 
+        internal static int[] InsertIntoMatrix(long numeroCarta) //TODO da rivedere insert
+        {
+            int[] MatrixCarta = new int[16];
+            string numCarta = numeroCarta.ToString();
+
+            for(int i =0; i< MatrixCarta.Length; i++)
+            {
+                MatrixCarta[i] = (int)numCarta[i];
+            }
+
+            return MatrixCarta;
+
+        }
+
+        internal static int[] DecryptCard(int[] matrixCarta, out int numeroDispari, out int totale)
+        {
+            int numeroRaddoppiato;
+            numeroDispari = 0;
+            totale = 0;
+            int[] MatrixCartaDecriptata = new int[matrixCarta.Length];
+            for(int i = 0; i<matrixCarta.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                       numeroRaddoppiato = matrixCarta[i] * 2;
+                    if (numeroRaddoppiato > 9)
+                    {
+                        numeroRaddoppiato = numeroRaddoppiato - 9;
+                    }
+                    MatrixCartaDecriptata[i] = numeroRaddoppiato;
+                    numeroDispari++;
+                }
+                else
+                {
+                    MatrixCartaDecriptata[i] = matrixCarta[i];
+
+                }
+
+                totale = totale + MatrixCartaDecriptata[i];
+               
+            }
+            return MatrixCartaDecriptata;
+
+        }
+
+    }
+}
---
+++ Esercizio2/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio2
+{
+    class Menu
+    {// Le carte di pagamento sono lunghe 16 cifre.
+     // Le prime 6 cifre rappresentano un numero di identificazione univoco per la banca che ha emesso la carta.
+        internal static void Start()
+        {   long numeroCarta;
+            int[] matrixCarta;
+            int numeriDispari;
+            int sommaCartaDecriptata;
+            Console.WriteLine("Inserisci le 16 cifre della carta da verificare: ");
+            while (!long.TryParse(Console.ReadLine(), out numeroCarta) || numeroCarta.ToString().Length != 16)
+            {
+                Console.WriteLine("Errore. Inserisci un numero di carta valido. Riprova: ");
+
+            }
+
+            //Inserisco il numero di carta in una matrice per gestire la verifica
+            matrixCarta = CardManager.InsertIntoMatrix(numeroCarta); //TODO: Da rivedere insertMatrix
+
+            //CardManager.PrintCard(numeroCarta);
+            //Ricerca prime 6 cifre = identificazione BANCA
+            //Ricerca 7-8 cifra = tipo di carta
+            //Ricerca 9-15 cifre = numero di serie carta
+
+            int[] matrixCartaDecriptata = CardManager.DecryptCard(matrixCarta, out numeriDispari, out sommaCartaDecriptata);
+            
+            printCarta(matrixCarta);
+
+            printCarta(matrixCartaDecriptata);
+
+            Console.WriteLine($"{sommaCartaDecriptata} + {numeriDispari} = {sommaCartaDecriptata + numeriDispari}");
+            if ((sommaCartaDecriptata + numeriDispari) % 10 == 0)
+                {
+                    Console.WriteLine("Numero della carta valido.");
+                }
+            else {
+                Console.WriteLine("Numero di carta non valido.");
+                 }
+
+
+            
+        }
+
+        private static void printCarta(int[] matrix)
+        {
+          foreach(var numero in matrix)
+            {
+                Console.Write(numero + "\t");
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
