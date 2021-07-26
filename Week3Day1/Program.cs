45e6390c0a1fb4f54e90cc8d66e3522f5c3d48aa
Angelica Cortez <angelica.cortez@hotmail.it>
2021-07-26 09:33:16 +0200

    Add project files.

---
+++ Week3Day1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Week3Day1
+{   // Dato un numero intero >= 0, cacolarne il fattoriale
+    // n! = n * (n-1) * (n-2) * .... 2 * 1
+    // n! = 1 * 2 * 3 * ... * n
+    // n! = 1 * (1+1) * (1+2) * ... * n
+
+    // n! = n * (n-1)!
+    // (n-1)! = (n-1) * (n-1-1)!
+
+    // Ma... 0! = 1
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int numero;
+            int fattoriale=1;
+            Console.WriteLine("Inserisci un numero maggiore o uguale a zero: ");
+            while(!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
+            {
+                Console.WriteLine("Inserisci un numero valido. Riprova: ");
+
+            }
+
+            for(int i = 0; i<=numero; i++)
+            {
+                fattoriale = fattoriale * i;
+            }
+
+            //fattoriale = 1;
+            //for(int i = numero; i > 0; i--)
+            //{
+            //    fattoriale = fattoriale * i;
+            //}
+        }
+
+        
+    }
+}
