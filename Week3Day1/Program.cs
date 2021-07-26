6da73888eaef3cd15871132fac7bf40e9df5681f
Angelica Cortez <angelica.cortez@hotmail.it>
2021-07-26 10:50:39 +0200

    Restructured Day1Week3

--- Week3Day1/Program.cs
+++ Week3Day1/Program.cs
@@ -1,30 +1,45 @@
 ﻿using System;
 
 namespace Week3Day1
-{   // Dato un numero intero >= 0, cacolarne il fattoriale
-    // n! = n * (n-1) * (n-2) * .... 2 * 1
-    // n! = 1 * 2 * 3 * ... * n
-    // n! = 1 * (1+1) * (1+2) * ... * n
-
-    // n! = n * (n-1)!
-    // (n-1)! = (n-1) * (n-1-1)!
-
-    // Ma... 0! = 1
+{   
     class Program
     {   
         static void Main(string[] args)
         {
+            //IterazioneVsRicorsione();
+            ValueVsReferenceType();
+        
+        }
+
+        private static void ValueVsReferenceType()
+        {
+            
+        }
+
+        private static void IterazioneVsRicorsione()
+        {
+            //ESEMPIO ESERCIZIO FATTORIALE
+            // Dato un numero intero >= 0, cacolarne il fattoriale
+            // n! = n * (n-1) * (n-2) * .... 2 * 1
+            // n! = 1 * 2 * 3 * ... * n
+            // n! = 1 * (1+1) * (1+2) * ... * n
+
+            // n! = n * (n-1)!
+            // (n-1)! = (n-1) * (n-1-1)!
+
+            // Ma... 0! = 1
+
             int numero;
-            int fattoriale=1;
+            int fattoriale = 1;
             Console.WriteLine("Inserisci un numero maggiore o uguale a zero: ");
-            while(!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
             {
                 Console.WriteLine("Inserisci un numero valido. Riprova: ");
 
             }
 
             //Iterazione
-            for(int i = 0; i<=numero; i++)
+            for (int i = 0; i <= numero; i++)
             {
                 fattoriale = fattoriale * i;
             }
@@ -37,7 +52,6 @@ namespace Week3Day1
 
             //Ricorsione
             int fattorialeRicorsione = FattorialeRicorsione(numero);
-        
         }
 
         //Ricorsione Metodo
